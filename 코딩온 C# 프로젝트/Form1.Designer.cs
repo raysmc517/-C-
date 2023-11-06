a906de3fe87aaf506c0b8bb034568d8f5a224ff0
chorysmind <raysmc517@naver.com>
2023-11-06 16:58:18 +0900

    Add project files.

---
+++ 코딩온 C# 프로젝트/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace 코딩온_C__프로젝트
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBOBOx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBOBOx
+            // 
+            this.textBOBOx.Location = new System.Drawing.Point(12, 32);
+            this.textBOBOx.Multiline = true;
+            this.textBOBOx.Name = "textBOBOx";
+            this.textBOBOx.Size = new System.Drawing.Size(751, 473);
+            this.textBOBOx.TabIndex = 0;
+            this.textBOBOx.Text = "텍스트 박스를 띄워라~!";
+            this.textBOBOx.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.textBOBOx);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBOBOx;
+    }
+}
+
---
+++ 코딩온 C# 프로젝트/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 코딩온_C__프로젝트
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           // MessageBox.Show("안녕하시긔??????");
+           /* 텍스트 출력*/
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBOBOx.Text = "이것은\r\n멀티라인\r\n텍스트박스";
+       }
+    }
+}
